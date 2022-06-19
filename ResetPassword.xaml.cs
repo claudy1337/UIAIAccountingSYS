05fc081e0fbc920c8383f3577d2f47a3641b1d96
claudy1337 <45602451+claudy1337@users.noreply.github.com>
2022-06-20 01:24:57 +0300

    Добавьте файлы проекта.

---
+++ ResetPassword.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFModernVerticalMenu
+{
+    /// <summary>
+    /// Логика взаимодействия для ResetPassword.xaml
+    /// </summary>
+    public partial class ResetPassword : Window
+    {
+        public ResetPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Auth auth = new Auth();
+            auth.Show();
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void BtnResetPass_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
